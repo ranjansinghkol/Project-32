6f027c8313c1ae860b95ef2e3668799ac8450de6
ranjansinghkol <38905611+ranjansinghkol@users.noreply.github.com>
2021-11-14 17:47:28 +0530

    Add files via upload

---
+++ classes/BlowerMouth.js
@@ -0,0 +1,24 @@
+class BlowerMouth {
+  constructor(x, y, w, h) {
+    var options = {
+      isStatic: true
+    };
+
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    this.w = w;
+    this.h = h;
+    this.color = color;
+    World.add(world, this.body);
+  }
+
+  show() {
+    var pos = this.body.position;
+
+    push();
+    translate(pos.x, pos.y);
+    rectMode(CENTER);
+    fill("#ff4500");
+    rect(0, 0, this.w, this.h);
+    pop();
+  }
+}
